eb8e016f9de739aa3697feec714e17f92818c479
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-01-31 09:37:39 -0700

    working on getting data from api

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,30 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import { Container, Row } from "reactstrap";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+const [char, charData] = useState([]);
   useEffect(() => {
+    Axios.get('https://rickandmortyapi.com/api/character/')
+    .then(res => {
+      console.log(res.data.results);
+      charData(res.data.results);
+    })
+    .catch(error => {
+      console.log('the data was not returned', error)
+    })
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>{char.map(people => {
+        return 
+      })}
+      {/* TODO: `array.map()` over your state here! */}
+      </h2>
     </section>
   );
 }
